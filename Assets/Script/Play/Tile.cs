06e5258ba8ef1df66a807ee44949cf40993e3abf
Jodongcheol <highcore444@gmail.com>
2023-12-13 19:06:00 +0900

    feat: 타일 교체 이동효과 추가 - 타일 선택시 MoveToward 부모 트랜스폼 이동

--- Assets/Script/Play/Tile.cs
+++ Assets/Script/Play/Tile.cs
@@ -4,7 +4,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using static UnityEngine.RuleTile.TilingRuleOutput;
+using Transform = UnityEngine.Transform;
 
 public class Tile : MonoBehaviour
 {
@@ -21,11 +22,12 @@ public class Tile : MonoBehaviour
     [SerializeField]
     private int Column;
     [SerializeField]
-    private GameObject DestroyEffect;
+    private GameObject DestroyEffectObject;
 
     [SerializeField]
     public TileType FruitType { get; set; }
     private bool bMatchCheck { get; set;}
+    private bool bMoveTile { get; set;}
 
     Tile() : base()
     {
@@ -37,10 +39,6 @@ public class Tile : MonoBehaviour
         int Tile1Col = _tile1.GetComponent<Tile>().Column;
         int Tile2Row = _tile2.GetComponent<Tile>().Row;
         int Tile2Col = _tile2.GetComponent<Tile>().Column;
-        
-        Vector3 Pos = _tile1.transform.position;
-        _tile1.transform.position = _tile2.transform.position;
-        _tile2.transform.position = Pos;
 
         Transform ParentTransform = _tile1.transform.parent;
         _tile1.transform.parent = _tile2.transform.parent;
@@ -51,9 +49,6 @@ public class Tile : MonoBehaviour
 
         _tile1.GetComponent<Tile>().SetPosition(Tile2Row, Tile2Col);
         _tile2.GetComponent<Tile>().SetPosition(Tile1Row, Tile1Col);
-
-        _tile1.transform.parent.parent.GetComponent<Board>().Check(Tile1Row, Tile1Col);
-        _tile2.transform.parent.parent.GetComponent<Board>().Check(Tile2Row, Tile2Col);
     }
     
     static public bool IsTileNearBy(GameObject _tile1, GameObject _tile2)
@@ -84,6 +79,7 @@ public class Tile : MonoBehaviour
     {
         Row = _Row;
         Column = _Column;
+        bMoveTile = true;
     }
     
     public void SetFruitType(TileType _Type)
@@ -104,22 +100,33 @@ public class Tile : MonoBehaviour
     void Start()
     {
         bMatchCheck = false;
+        bMoveTile = false;
     }
 
     void Update()
     {
+        if (bMoveTile)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transform.parent.position, Time.deltaTime * 10.0f);
+
+            if (Mathf.Approximately(transform.position.x, transform.parent.position.x) && 
+                Mathf.Approximately(transform.position.y, transform.parent.position.y))
+            {
+                bMoveTile = false;
+
+                transform.parent.parent.GetComponent<Board>().Check(Row, Column);
+            }
+        }
+
         if (bMatchCheck)
         {
             TileBackground TileBack = this.transform.parent.gameObject.GetComponent<TileBackground>();
 
             TileBack.CreateFruit();
 
+            GameObject EffectObject = Instantiate(DestroyEffectObject, this.transform.parent);
+            Destroy(EffectObject, 1.0f);
             Destroy(this.gameObject);
         }
-    }
-
-    private void OnDestroy()
-    {
-        Destroy(this.gameObject);
     }
 }
